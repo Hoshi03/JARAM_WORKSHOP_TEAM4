5d17bd9152651c7dbe0f345ecb8ec76374e35653
jungchoib <jungchoi1@naver.com>
2023-07-27 13:46:27 +0900

    추가 전송 글자를 반대로 한거같긴한데 재료 추가와 GPT에게 text로 보내는 버튼을 나누었습니다.

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ dotenv.config();
 var request = require('request');
 var { OpenAIApi, Configuration } = require('openai');
 app.use(express.static('public'));
-app.set('OPENAI_API_KEY', process.env.OPENAI_API_KEY) ;
+
+// app.set('OPENAI_API_KEY', process.env.OPENAI_API_KEY) ;
 
 // console.log(process.env.OPENAI_API_KEY);
 
@@ -19,17 +20,17 @@ let config = new Configuration({
 let openai = new OpenAIApi(config);
 
 
-
 app.get('/', function(req,res){
   res.sendFile(__dirname + '/index.html')
 })
+
+let ingredients = [];
 
 var client_id = '5v7pE3U7G_m0Eiovkp5b';
 var client_secret = '2u3g2o6Ppw';
 
 ////////////////////////SQLite 데이터베이스를 생성하고 words 테이블생성
 const sqlite3 = require('sqlite3').verbose();
-
 // 데이터베이스 생성 및 연결
 const db = new sqlite3.Database('my_database.db');
 
@@ -43,58 +44,58 @@ db.run(`CREATE TABLE IF NOT EXISTS words (
 app.get('/translate', function (req, res) {
    var api_url = 'https://openapi.naver.com/v1/papago/n2mt';
    var query = req.query.q;
+
+///////////////// 입력받은 단어를 데이터베이스에 저장
+    db.run(`INSERT INTO words (word) VALUES (?)`, [query], function (err) {
+      if (err) {
+        return console.log('데이터베이스 저장 오류:', err.message);
+      }
+      console.log('단어가 성공적으로 저장되었습니다.');
+    });
+/////////////////
    
-     // 입력받은 단어를 데이터베이스에 저장
-  db.run(`INSERT INTO words (word) VALUES (?)`, [query], function (err) {
-    if (err) {
-      return console.log('데이터베이스 저장 오류:', err.message);
-    }
-    console.log('단어가 성공적으로 저장되었습니다.');
-  });
+   var options = {
+       url: api_url,
+       form: {'source':'ko', 'target':'en', 'text':query},
+       headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret}
+    };
+   request.post(options, function (error, response, body) {
+       var 영어 = JSON.parse(body).message?.result.translatedText;
 
-  //  var options = {
-  //      url: api_url,
-  //      form: {'source':'ko', 'target':'en', 'text':query},
-  //      headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret}
-  //   };
-  //  request.post(options, function (error, response, body) {
-  //      var 영어 = JSON.parse(body).message?.result.translatedText;
+       openai.createCompletion({
+          model: "text-davinci-002",
+          prompt: 영어,
+          temperature: 0.7,
+          max_tokens: 128,
+          top_p: 1,
+          frequency_penalty: 0,
+          presence_penalty: 0,
+        }).then((result) => {
+          console.log('ai 응답', result.data.choices[0].text);
 
-  //      openai.createCompletion({
-  //         model: "text-davinci-002",
-  //         prompt: 영어,
-  //         temperature: 0.7,
-  //         max_tokens: 128,
-  //         top_p: 1,
-  //         frequency_penalty: 0,
-  //         presence_penalty: 0,
-  //       }).then((result) => {
-  //         console.log('ai 응답', result.data.choices[0].text);
-
-  //         var api_url = 'https://openapi.naver.com/v1/papago/n2mt';
-  //         var query = result.data.choices[0].text;
-  //         var options = {
-  //             url: api_url,
-  //             form: {'source':'en', 'target':'ko', 'text':query},
-  //             headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret}
-  //         };
-  //         request.post(options, function (error, response, body) {
-  //           console.log(body);
-  //           res.status(200).json(body);
+          var api_url = 'https://openapi.naver.com/v1/papago/n2mt';
+          var query = result.data.choices[0].text;
+          var options = {
+              url: api_url,
+              form: {'source':'en', 'target':'ko', 'text':query},
+              headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret}
+          };
+          request.post(options, function (error, response, body) {
+            console.log(body);
+            res.status(200).json(body);
               
-  //         });
+          });
 
 
-  //       }).catch((error)=>{
-  //         console.log('openai error', error)
-  //       })
+        }).catch((error)=>{
+          console.log('openai error', error)
+        })
 
-  //  });
+   });
  });
 
- 
  ////////////////// 예시: '/words' 엔드포인트에서 모든 단어를 불러오는 코드
-app.get('/words', function (req, res) {
+ app.get('/words', function (req, res) {
   db.all(`SELECT * FROM words`, function (err, rows) {
     if (err) {
       return console.log('데이터베이스 조회 오류:', err.message);
@@ -102,8 +103,7 @@ app.get('/words', function (req, res) {
     res.status(200).json(rows);
   });
 });
-
-// Endpoint to delete a word by ID
+//////////////////// Endpoint to delete a word by ID
 app.delete('/words/:id', function (req, res) {
   const wordId = req.params.id;
 
@@ -115,17 +115,13 @@ app.delete('/words/:id', function (req, res) {
     res.sendStatus(200);
   });
 });
+///////////////////
 
-
-
-
-
-
-
-
-
-
-
-app.listen(3000, function () {
-  console.log('http://localhost:3000/ app listening on port 3000!');
+ // '/ingredients' 엔드포인트를 통해 입력한 재료 배열 반환
+app.get('/ingredients', function (req, res) {
+  res.status(200).json(ingredients);
 });
+
+ app.listen(3000, function () {
+   console.log('http://localhost:3000/ app listening on port 3000!');
+ });
